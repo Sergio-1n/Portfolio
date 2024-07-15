5215fd2ff0b7742a27cf3543fa38586ca0418e7e
Sergio-1n <153386769+Sergio-1n@users.noreply.github.com>
2024-07-15 20:05:34 +0200

    Add files via upload

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 
 import { AnimatePresence, motion } from 'framer-motion';
 import { CanvasRevealEffect } from './ui/CanvasRevealEffect';
-import { div } from 'three/examples/jsm/nodes/Nodes.js';
 
 export function Approach() {
   return (
